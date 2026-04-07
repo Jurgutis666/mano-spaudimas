ccf9fa749a7a0a887250c14888d08c7597db318a
JurgisSakalauskas <89006769+JurgisSakalauskas@users.noreply.github.com>
2026-04-07 09:28:26 +0300

    Initial commit: svari React struktura ir isvalytas kodas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Kraujospūdžio Seklys</h1>
+      <p>Sveiki atvykę į savo sveikatos valdymo sistemą.</p>
+
+      {/* Čia vėliau atsiras mūsų komponentai */}
+    </div>
+  );
+}
+
+export default App;
