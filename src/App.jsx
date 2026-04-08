840bbdd18fac6450675be8799af732cf74954bf2
JurgisSakalauskas <89006769+JurgisSakalauskas@users.noreply.github.com>
2026-04-08 15:21:43 +0300

    local storage created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import MeasurementForm from "./components/MeasurementForm";
 import "./index.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MeasurementList from "./components/MeasurementList";
 
 function App() {
-  const [records, setRecords] = useState([]);
+  const [records, setRecords] = useState(() => {
+    const savedData = localStorage.getItem("my-data");
+    return savedData ? JSON.parse(savedData) : [];
+  });
 
   function addRecord(record) {
     setRecords([...records, record]);
   }
+
+  useEffect(() => {
+    const stringifiedData = JSON.stringify(records);
+    localStorage.setItem("my-data", stringifiedData);
+  }, [records]);
+
   return (
     <div className="app-container">
       <h1>Kraujospūdžio Seklys</h1>
